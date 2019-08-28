dfcb205cd6938c784e3d3ea961f1a308ad8130d1
Lucas Zanek <lucaszanek94@gmail.com>
2019-08-27 22:33:33 -0300

    [Back] Added basic routes and config

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {useNewUrlParser:true});
+        console.log("Mongo db conencted");
+    } catch (error) {
+        console.error(error.message);
+        // Exit process with failure
+        process.exit(1);
+    }   
+}
+
+module.exports = connectDB;     
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db')
+
+//  Connect database
+connectDB();
 
 const app = express();
 app.get('/', (req,res) => res.send('API running'))
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
 
 const PORT = process.env.PORT || 5000  
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
