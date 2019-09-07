3e546783699332b8de925c63ef4addaeab642945
Lucas Zanek <lucaszanek94@gmail.com>
2019-09-07 12:54:14 -0300

    [BackEnd] Added users schema model, added validations to the request using express validator and parsed the json

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const {check,validationResult} = require('express-validator/check');
 
 // @route  GET api/users
-// @desc   Test route
+// @desc   Register user 
 // @access Public
-router.get('/',(req,res) => res.send('User route'))
+
+router.post('/',
+[
+    //Check the fields recieved from the post request from users.
+    check('name','Name is required').not().isEmpty(),
+    check('email','Please include a valid email').isEmail(),
+    check('password','Please include a password with 6 or more characters').isLength({
+        min:6,
+        max:15
+    })
+],
+(request,response) => {
+    // Run and check the validations, if errors is not empty return the error array
+    const errors = validationResult(request);
+    if(!errors.isEmpty()){
+        return response.status(400).json({errors:errors.array()});
+    }
+    console.log(request.body)
+    response.send('User route')
+
+})
 
 module.exports = router;    
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const connectDB = require('./config/db')
 
+const app = express();
 //  Connect database
 connectDB();
 
-const app = express();
+// Init middleware
+app.use(express.json({extended:false}))
+
+
 app.get('/', (req,res) => res.send('API running'))
 
 // Define routes
