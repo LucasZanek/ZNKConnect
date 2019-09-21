f5072fb87a8a1b3a3372d4e255454bc1d5ac3259
Lucas Zanek <lucaszanek94@gmail.com>
2019-09-21 16:14:40 -0300

    [BackEnd] Profile model fixed, added profile/me routh protected. Repsonse withouth profile and error

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth')
 
-// @route  GET api/profile
-// @desc   Test route
-// @access Public
-router.get('/',(req,res) => res.send('Profile route'))
+const User = require('../../models/User');
+const Profile = require('../../models/Profile')
+
+// @route  GET api/profile/me
+// @desc   Get current users profile 
+// @access Private
+
+router.get('/me', auth, async (request,response) => {
+    try {
+        // Search by user id from token and populate
+        const userProfile = await Profile.findOne({user: request.user.id}).populate('user',['name' , 'avatar']);
+        if(!userProfile){
+            return response.status(400).json([{msg:"There isn't profile for this user"}]);
+        }
+        
+         response.json({profile: userProfile});
+
+    } catch (error) {
+        console.error(error.message);
+        response.status(500).send('Server error');
+    }
+})
 
 module.exports = router;    
